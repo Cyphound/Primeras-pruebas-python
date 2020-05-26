e9d5d111bae37caa54b5635cac37e16eb40b599b
Soter257 <51380838+Soter257@users.noreply.github.com>
2020-05-26 14:16:38 -0400

    Add files via upload

---
+++ Prueba1.py
@@ -0,0 +1,11 @@
+
+# print imprime un mensaje en pantalla y corresponde a una funcion
+# poner un "#" y luego un mensaje es para poder verlo sin que afecte al programa
+print("Hola mundo")
+
+# esto (palabras) es un dato tipo string o cadena y se puede poner de muchas maneras
+print("Hola mundo")
+print('Hola mundo')
+print("""Hola mundo""")
+print('''Hola mundo''')
+# todas estas tienen distintas funciones. aunque en consola lo veas igual
---
+++ Prueba2.py
@@ -0,0 +1,51 @@
+
+# Tipos de datos
+
+# String: Cadena de palabras
+print("Hello World")
+
+# Integer: Numeros enteros
+print(4)
+
+# Float: Numeros decimales
+print(3.5)
+
+# Boolean: Define dos estados
+True
+False
+
+# List: Crea una lista de datos (Estos se pueden cambiar)
+[45, 50 ,23 ,30]
+['Hello', 'Hi', 'Hola']
+[25, "Hi", True, 2.4]
+
+# Tuples: Crea una lista de datos (Estos NO se pueden cambiar) (Se le conoce como inmutable)
+(45, 59, 23, 30)
+('Hello', 'Hi', 'Hola')
+(25, "Hi", True, 2.4)
+
+# Dictorionies: Agrupan datos de una entidad
+{
+    "nombredelapersona":"Ryan",
+    "apellido":"Ray",
+    "apodo":"Soter"
+}
+{
+	"dinero2019":540000,
+	"dinero2020":120000
+}
+
+# None: No tiene ningún tipo de dato
+None
+
+# si pones el dato que quieras adelante de un "type" te dira que clases es
+type("Hello world")
+type("100")
+# para ver este dato en pantalla se debe anteponer un print
+print(type("Hola mundo"))
+
+# para unir dos string se puede usar un "+" (a esto se le conoce como concatenar)
+print("Hello" + " World")
+
+# tambien se pueden juntar otras clases de la misma forma
+print("2" + "4")
